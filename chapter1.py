f4c000d8d167570240e1e3fc5b2c8a5707d1d6d5
Jay konn <jayaprakash.konnur@gmail.com>
2025-10-16 23:32:32 -0400

    Add initial implementation of chapter1.py with basic string operations and print statements

---
+++ chapter1.py
@@ -0,0 +1,17 @@
+
+A='jayaprakash'
+B=f'{A} konnur'
+print (A,B,'Engineer','python')
+C=A+' '+B
+print (C)
+D= len (C)
+print (D)
+print (A*3)
+E=A*3
+print (E)
+dasmdnbsadkas = f'My name iasfhas0231434324 lkajdas is {A+B}  ,   {D}      {B}'
+#print (dasmdnbsadkas)
+
+city='Bengaluru'
+pop=5000
+print ('My City Name is'+city+'and the population is'+50000)
